3a754d9339ae9262eb7f403a32a55584827a216c
escobarlagunaf <escobarlagunaf@gmail.com>
2025-06-02 15:45:32 -0500

    adding divide feature

--- src/main/java/edu/unac/Calculator.java
+++ src/main/java/edu/unac/Calculator.java
@@ -12,4 +12,11 @@ public class Calculator {
     public int subtract(int a, int b) {
         return a - b;
     }
+
+    public int divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero.");
+        }
+        return a / b;
+    }
 }
