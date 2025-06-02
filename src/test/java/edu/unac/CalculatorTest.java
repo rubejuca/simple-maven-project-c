aeb24681d24510e80a07308fa11b2f056f94fab0
escobarlagunaf <escobarlagunaf@gmail.com>
2025-06-02 16:39:55 -0500

    testing

--- src/test/java/edu/unac/CalculatorTest.java
+++ src/test/java/edu/unac/CalculatorTest.java
@@ -10,4 +10,10 @@ class CalculatorTest {
         Calculator calc = new Calculator();
         assertEquals(5, calc.add(2, 3));
     }
+
+    @Test
+    void testMultiplication() {
+        Calculator calc = new Calculator();
+        assertEquals(6, calc.multiply(2, 3));
+    }
 }
