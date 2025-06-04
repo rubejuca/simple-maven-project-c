8deba5cb4c63727b9b2321824edf2e414e68a043
Juan Camilo Rubio <rubejuca@gmail.com>
2025-06-03 19:35:03 -0500

    test

--- src/test/java/edu/unac/CalculatorTest.java
+++ src/test/java/edu/unac/CalculatorTest.java
@@ -5,4 +5,25 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+
+  @Test
+  void  sum() {
+    Calculator calculator = new Calculator();
+    int result = calculator.add(1, 2);
+    assertEquals(3, result);
+  }
+
+  @Test
+  void  subtract() {
+    Calculator calculator = new Calculator();
+    int result = calculator.subtract(1, 2);
+    assertEquals(-1, result);
+  }
+
+  @Test
+  void  multiply() {
+    Calculator calculator = new Calculator();
+    int result = calculator.multiply(1, 2);
+    assertEquals(2, result);
+  }
 }
